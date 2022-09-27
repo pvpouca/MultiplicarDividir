23b5d9a58c4f3446b4a3c1d9c4a2ece47add6791
pvpouca <113592523+pvpouca@users.noreply.github.com>
2022-09-27 11:12:44 +0100

    Add files via upload

---
+++ multiplicacao_divisao/index.js
@@ -0,0 +1,11 @@
+function multiplicar(){
+    let num1 = document.getElementById('N1').value;
+    let num2 = document.getElementById('N2').value;
+    document.getElementById('Resultado').textContent=num1*num2
+}
+
+function dividir(){
+    let num1 = document.getElementById('N1').value;
+    let num2 = document.getElementById('N2').value;
+    document.getElementById('Resultado').textContent=num1/num2
+}
